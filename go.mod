0a5611f6eeeb939dff9256b4298e2cbdfd5f6ba4
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-11-08 11:26:05 +0100

    feat: Try to use go.lsp.dev for very basic LSP.

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/yeldiRium/hledger-language-server
 
 go 1.23.1
+
+require (
+	go.lsp.dev/jsonrpc2 v0.10.0
+	go.lsp.dev/protocol v0.12.0
+)
+
+require (
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentio/asm v1.1.3 // indirect
+	github.com/segmentio/encoding v0.3.4 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	go.lsp.dev/pkg v0.0.0-20210717090340-384b27a52fb2 // indirect
+	go.lsp.dev/uri v0.3.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"io"
+	"os"
+	"time"
+
+	"go.lsp.dev/jsonrpc2"
+	"go.lsp.dev/protocol"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	connection := jsonrpc2.NewConn(jsonrpc2.NewStream(&rwCloser{os.Stdin, os.Stdout}))
+
+	handler, ctx, err := NewHandler(context.Background(), protocol.ServerDispatcher(connection, nil))
+	if err != nil {
+		panic(err)
+	}
+
+	connection.Go(ctx, protocol.ServerHandler(handler, jsonrpc2.MethodNotFoundHandler))
+	<-connection.Done()
+}
+
+type Handler struct {
+	protocol.Server
+}
+
+func (h Handler) Initialize(ctx context.Context, params *protocol.InitializeParams) (*protocol.InitializeResult, error) {
+	return &protocol.InitializeResult{
+		Capabilities: protocol.ServerCapabilities{
+			HoverProvider: true,
+		},
+		ServerInfo: &protocol.ServerInfo{
+			Name:    "hledger-language-server",
+			Version: "0.0.1",
+		},
+	}, nil
+}
+
+func (h Handler) Hover(ctx context.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
+	return &protocol.Hover{
+		Contents: protocol.MarkupContent{
+			Kind:  protocol.Markdown,
+			Value: "Hello, world!",
+		},
+	}, nil
+}
+
+func NewHandler(ctx context.Context, server protocol.Server) (Handler, context.Context, error) {
+	// Do initialization logic here, including
+	// stuff like setting state variables
+	// by returning a new context with
+	// context.WithValue(context, ...)
+	// instead of just context
+	return Handler{Server: server}, ctx, nil
+}
+
+type rwCloser struct {
+	io.ReadCloser
+	io.WriteCloser
+}
+
+// SetWriteDeadline implements rpc.Conn.
+func (rw rwCloser) SetWriteDeadline(time.Time) error {
+	return nil
+}
+
+func (rw rwCloser) Close() error {
+	err := rw.ReadCloser.Close()
+	if err != nil {
+		return err
+	}
+	err = rw.WriteCloser.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
