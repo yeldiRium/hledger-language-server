badb0d5b8518c961bdced70c04213d2ca23cfa9f
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-11-09 11:14:47 +0100

    feat: Find word around cursor and show it in hover.

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
+	"strings"
 	"time"
+	"unicode"
 
 	"go.lsp.dev/jsonrpc2"
 	"go.lsp.dev/protocol"
@@ -41,14 +44,46 @@ func (h Handler) Initialize(ctx context.Context, params *protocol.InitializePara
 	}, nil
 }
 
+func WordAroundCursor(line string, position int) string {
+	wordBegin := strings.LastIndexFunc(line[:position], unicode.IsSpace) + 1
+	if wordBegin == -1 {
+		wordBegin = 0
+	}
+
+	wordEnd := strings.IndexFunc(line[position:], unicode.IsSpace)
+	if wordEnd == -1 {
+		wordEnd = len(line)
+	} else {
+		wordEnd = wordEnd + position
+	}
+
+	word := line[wordBegin:wordEnd]
+
+	return word
+}
+
 func (h Handler) Hover(ctx context.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
-	h.logger.Sugar().Infof("hovering over %s", params.TextDocument.URI)
+	h.logger.Sugar().Infof("hovering over %s in line %d at position %d", params.TextDocument.URI, params.Position.Line, params.Position.Character)
+
+	fileContent, err := os.ReadFile(params.TextDocument.URI.Filename())
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(fileContent), "\n")
+	line := lines[params.Position.Line]
+	word := WordAroundCursor(line, int(params.Position.Character))
+
 	return &protocol.Hover{
 		Contents: protocol.MarkupContent{
 			Kind:  protocol.Markdown,
-			Value: "Hello, world!",
+			Value: fmt.Sprintf("You're hovering over \"%s\"", word),
 		},
 	}, nil
+}
+
+func (h Handler) Shutdown(ctx context.Context) error {
+	return nil
 }
 
 func NewHandler(ctx context.Context, server protocol.Server, logger *zap.Logger) (Handler, context.Context, error) {
