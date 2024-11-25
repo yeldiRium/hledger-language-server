a261afc77e3501884e2153f053244e3839277902
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-11-25 19:41:20 +0100

    feat: Add cmd that parses a journal file and prints its AST as json

---
+++ cmd/parse-file/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/yeldiRium/hledger-language-server/ledger"
+)
+
+func main() {
+	parser := ledger.MakeJournalParser()
+	tokens, err := parser.Lex("stdin", os.Stdin)
+	if err != nil {
+		fmt.Printf("An error occured: %v\n\n", err)
+		fmt.Printf("Maybe the parser still found something? Here are the tokens:\n\n")
+	}
+
+    output, err := json.Marshal(tokens)
+    if err != nil {
+        fmt.Printf("Failed to convert AST to json. Here is the raw AST:\n\n")
+        fmt.Printf("%#v\n", tokens)
+    }
+	fmt.Printf("%s\n", output)
+}
