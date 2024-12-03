a41299eb9979679231480d0f3c7a50d657705598
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-12-03 17:29:29 +0100

    chore: Log to temp file for debugging

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io"
 	"os"
+	"path/filepath"
 	"time"
 
 	"go.lsp.dev/jsonrpc2"
@@ -14,7 +15,17 @@ import (
 )
 
 func main() {
-	logger, _ := zap.NewDevelopmentConfig().Build()
+	dir, err := os.MkdirTemp("", "hledger-language-server")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+	logFile := filepath.Join(dir, "log.txt")
+
+	zapConfig := zap.NewDevelopmentConfig()
+	zapConfig.OutputPaths = []string{logFile}
+	zapConfig.ErrorOutputPaths = []string{logFile}
+	logger, _ := zapConfig.Build()
 	connection := jsonrpc2.NewConn(jsonrpc2.NewStream(&rwCloser{os.Stdin, os.Stdout}))
 
 	client := protocol.ClientDispatcher(connection, logger)
@@ -26,7 +37,6 @@ func main() {
 	connection.Go(ctx, protocol.ServerHandler(handler, jsonrpc2.MethodNotFoundHandler))
 	<-connection.Done()
 }
-
 
 type rwCloser struct {
 	io.ReadCloser
