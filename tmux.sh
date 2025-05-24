0ad0c1d42a27ad35f78675c136ad6297c1faec5e
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-05-24 15:36:02 +0200

    chore: Extend tmux script with git bug

--- tmux.sh
+++ tmux.sh
@@ -13,17 +13,25 @@ attach() {
 if ! tmux has-session -t "${session}" 2>/dev/null; then
   tmux new-session -d -s "${session}" -x "$(tput cols)" -y "$(tput lines)"
 
-  tmux split-window -t "${session}:0.0" -v
-  tmux resize-pane -t "${session}:0.0" -y "30%"
+  tmux split-window -t "${session}:0.0" -h -l "40%"
 
   sleep 1
 
-  tmux send-keys -t "${session}:0.0" "devbox shell" "C-m"
-  tmux send-keys -t "${session}:0.1" "devbox shell" "C-m"
+  tmux send-keys -t "${session}:0.0" " devbox shell" "C-m"
+  tmux send-keys -t "${session}:0.1" " devbox shell" "C-m"
 
-  tmux send-keys -t "${session}:0.0" "nvim" "C-m"
+  if [ $(git bug 1>&2 2>/dev/null)$? -eq 0 ]; then
+    tmux split-window -t "${session}:0.1" -v -l "20%" -b -d
+
+    tmux send-keys -t "${session}:0.1" " git bug pull" "C-m"
+    tmux send-keys -t "${session}:0.1" " git bug termui" "C-m"
+  fi
+
+  tmux send-keys -t "${session}:0.0" " nvim" "C-m"
 
   tmux select-window -t "${session}:0"
+  tmux select-pane -t "${session}:0.0"
 fi
 
 attach
+
